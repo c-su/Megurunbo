1e60572bef036aff213a1f950b2a4174d5d07723
Yukihiro Moriyama <m.y.like.music.10@gmail.com>
2014-09-15 12:34:18 +0900

    SPARQLクライアント完成

--- js/SPARQLClient.js
+++ js/SPARQLClient.js
@@ -1,45 +1,59 @@
 function SPARQLClient() {
     this.rdfmgr = new RDFmgr();
+    this.projectID = 'megurunbo';
 }
 
-SPARQLClient.prototype.selectSPARQLQuery = function(num) {
-    var query;
-
-    switch (num) {
-        case 1:
-            query = 'select * where {' +
-                '?s rdfs:label ?label.' +
-                '?s <http://linkdata.org/property/rdf1s1871i#food> ?food.' +
-                'optional {?s dce:description ?des.}' +
-                '}';
-            break;
-        case 2:
-            query = 'select * where {' +
-                '?s rdfs:label ?label.' +
-                '?s <http://linkdata.org/property/rdf1s1871i#place_name> ?place_name.' +
-                'optional {?s dce:description ?des.}' +
-                '}';
-            break;
-        case 3:
-            query = 'select * where {' +
-                '?s rdfs:label ?label.' +
-                'optional {?s <http://linkdata.org/property/rdf1s1871i#food> ?food.}' +
-                'optional {?s <http://linkdata.org/property/rdf1s1871i#place> ?place.}' +
-                '}';
-            break;
-
-        default:
-            alert('Select Error');
-            break;
-    }
-    this.sparql = query;
+SPARQLClient.prototype.setKomaQuery = function(from, to) {
+    this.sparql = 'select * where {' +
+        '?s rdfs:label ?label.' +
+        'optional {?s <http://linkdata.org/property/rdf1s1871i#food> ?food.}' +
+        'optional {?s <http://linkdata.org/property/rdf1s1871i#place> ?place.}' +
+        'filter (?koma >= ' + from + ' && ?koma <= ' + to + ')' +
+        '}';
+    this.projectID = 'megurunbo';
 };
 
-SPARQLClient.prototype.request = function(success, prjectID) {
+SPARQLClient.prototype.setFoodQuery = function(array) {
+    var tmp = "";
+    for (var i = 0; i < array.length; i++) {
+        tmp += '?food = "' + array[i] + '"@ja';
+        if (i != array.length - 1) tmp += "||";
+    }
+
+    this.sparql = 'select * where {' +
+        '?s rdfs:label ?label.' +
+        '?s <http://linkdata.org/property/rdf1s1871i#food> ?food.' +
+        'optional {?s dce:description ?des.}' +
+        'filter(' + tmp + ')' +
+        '}';
+
+    this.projectID = 'megurunbo_food';
+    console.log(this.sparql);
+};
+
+SPARQLClient.prototype.setPlaceQuery = function(array) {
+    var tmp = "";
+    for (var i = 0; i < array.length; i++) {
+        tmp += '?place_name = "' + array[i] + '"@ja';
+        if (i != array.length - 1) tmp += "||";
+    }
+
+    this.sparql = 'select * where {' +
+        '?s rdfs:label ?label.' +
+        '?s <http://linkdata.org/property/rdf1s1871i#place_name> ?place_name.' +
+        'optional {?s dce:description ?des.}' +
+        'filter(' + tmp + ')' +
+        '}';
+
+    this.projectID = 'megurunbo_food';
+    console.log(this.sparql);
+};
+
+SPARQLClient.prototype.request = function(success) {
     this.rdfmgr.executeSparql({
         sparql: this.sparql,
         inference: false,
-        projectID: projectID,
+        projectID: this.projectID,
         success: success,
         error: function() {
             console.log('error');
--- js/sketch_mouse.js
+++ js/sketch_mouse.js
@@ -14,99 +14,107 @@ var img_h = 0.0;
 //var koma_click_num = 0;
 //var button_size = 0.0;
 var button;
-//var userAgent = window.navigator.userAgent.toLowerCase();  
+//var userAgent = window.navigator.userAgent.toLowerCase();
 
 function preload() {
-	file = loadStrings("./csv/koma_info.csv");
+    file = loadStrings("./csv/koma_info.csv");
 }
 
 function setup() {
-	var i, j;
-	var tmp = [];
+    var i, j;
+    var tmp = [];
 
-	if(is_iphone5()){
-	 	createCanvas(960, 1365);
-	}else{
-		createCanvas(windowWidth-20, windowHeight);
-	}
-	button = new Button();
-	img_w = width * 3 / 2;
-	resize_rate = img_w / img_origin_w;
-	img_h = img_origin_h * resize_rate;
-	//button_size = width / 7;
-	speed_rate = 1/resize_rate;
-	frameRate(15);
-	textSize(32);
-	for (i = 0; i < page_len; i++) {
-  	page[i] = new Page("./img/page/page" + i + ".jpg", img_w, img_h, speed_rate);
-	}
+    if (is_iphone5()) {
+        createCanvas(960, 1365);
+    } else {
+        createCanvas(windowWidth - 20, windowHeight);
+    }
+    button = new Button();
+    img_w = width * 3 / 2;
+    resize_rate = img_w / img_origin_w;
+    img_h = img_origin_h * resize_rate;
+    //button_size = width / 7;
+    speed_rate = 1 / resize_rate;
+    frameRate(15);
+    textSize(32);
+    for (i = 0; i < page_len; i++) {
+        page[i] = new Page("./img/page/page" + i + ".jpg", img_w, img_h, speed_rate);
+    }
 
-	for (i = 0; i < file.length; i++) {
-		tmp = file[i].split(",");
-		data[i] = [];
-		for (j = 0; j < tmp.length; j++) {
-			data[i][j] = tmp[j];
-		}
-	}
+    for (i = 0; i < file.length; i++) {
+        tmp = file[i].split(",");
+        data[i] = [];
+        for (j = 0; j < tmp.length; j++) {
+            data[i][j] = tmp[j];
+        }
+    }
 
-	for (i = 1; i < file.length; i++) {
-		page[parseInt(data[i][0])].koma_add(
-				parseInt(data[i][1]),
-				Math.floor(parseInt(data[i][2]) * resize_rate), Math.floor(parseInt(data[i][3]) * resize_rate),
-				Math.floor(parseInt(data[i][4]) * resize_rate), Math.floor(parseInt(data[i][5]) * resize_rate)
-		);
-	}
+    for (i = 1; i < file.length; i++) {
+        page[parseInt(data[i][0])].koma_add(
+            parseInt(data[i][1]),
+            Math.floor(parseInt(data[i][2]) * resize_rate), Math.floor(parseInt(data[i][3]) * resize_rate),
+            Math.floor(parseInt(data[i][4]) * resize_rate), Math.floor(parseInt(data[i][5]) * resize_rate)
+        );
+    }
+
+    var sparqlClient = new SPARQLClient();
+    sparqlClient.setPlaceQuery(['感応院']);
+    sparqlClient.request(function(re) {
+        var test = re.getJson();
+        console.log(test);
+    });
+
 }
 
 function draw() {
-	var i, j;
-	background(255);
-	page[page_num].stop_at_edge();
-	page[page_num].display();
-	button.display();
+    var i, j;
+    background(255);
+    page[page_num].stop_at_edge();
+    page[page_num].display();
+    button.display();
 
-	// for testing
-	//fill(0);
-	//text("W:"+width+", H"+height, 30, 30);
-	//text("Speed:"+speed_rate, 30, 60); 
-	//text("Touch?:"+is_touch_device, 30, 60);
-	//text("Page:"+page_num+", Koma:"+koma_click_num, 30, 90);
-	
+    // for testing
+    //fill(0);
+    //text("W:"+width+", H"+height, 30, 30);
+    //text("Speed:"+speed_rate, 30, 60);
+    //text("Touch?:"+is_touch_device, 30, 60);
+    //text("Page:"+page_num+", Koma:"+koma_click_num, 30, 90);
+
 }
 
 function mousePressed() {
-	tap_start_x = mouseX;
-	tap_start_y = mouseY;
+    tap_start_x = mouseX;
+    tap_start_y = mouseY;
 }
 
 function mouseDragged() {
-	page[page_num].move(mouseX, mouseY, tap_start_x, tap_start_y);
+    page[page_num].move(mouseX, mouseY, tap_start_x, tap_start_y);
 }
 
-function mouseReleased() {	
-	if(is_tap()){
-	  if (button.is_left_tapped(mouseX, mouseY)) {
-	    if (page_num !== page_len - 1) {
-	      page_num++;
-	      page[page_num].init();
-	    }
-	  // Right Button
-	  } else if (button.is_right_tapped(mouseX, mouseY)) {
-	    if (page_num !== 0) {
-	      page_num--;
-	      page[page_num].init();
-	    }
-	  } else {
-	    page[page_num].clicked_koma_num(mouseX, mouseY);
-	  }
-	}
-	
+function mouseReleased() {
+    if (is_tap()) {
+        if (button.is_left_tapped(mouseX, mouseY)) {
+            if (page_num !== page_len - 1) {
+                page_num++;
+                page[page_num].init();
+            }
+            // Right Button
+        } else if (button.is_right_tapped(mouseX, mouseY)) {
+            if (page_num !== 0) {
+                page_num--;
+                page[page_num].init();
+            }
+        } else {
+            page[page_num].clicked_koma_num(mouseX, mouseY);
+        }
+    }
+
 }
 
-function is_tap(){
-  return (tap_start_x == mouseX && tap_start_y == mouseY);
+function is_tap() {
+    return (tap_start_x == mouseX && tap_start_y == mouseY);
 }
 
-function is_iphone5(){
- return displayHeight===568;
+function is_iphone5() {
+    return displayHeight === 568;
 }
