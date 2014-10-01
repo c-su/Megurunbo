1c0d017ab006955d51ffd116bda631d921f4c7ce
Yukihiro Moriyama <m.y.like.music.10@gmail.com>
2014-10-01 15:41:52 +0900

    SPARQLClientのコメントを記入

--- js/SPARQLClient.js
+++ js/SPARQLClient.js
@@ -1,8 +1,13 @@
+/* sparqlEPCU のプロジェクトからめぐるんぼで使用するデータを取得するためのクライアントオブジェクト */
 function SPARQLClient() {
     this.rdfmgr = new RDFmgr();
     this.projectID = 'megurunbo';
 }
 
+/**
+ * 美味しんぼのコマに付随している情報を取得するためのSPARQLクエリの生成
+ * @param {Object} koma どこからどこまでのコマを指定するのか
+ */
 SPARQLClient.prototype.setKomaQuery = function(komaFirstLast) {
     this.sparql = 'select * where {' +
         '?s <http://linkdata.org/property/rdf1s1871i#koma> ?koma.' +
@@ -13,6 +18,10 @@ SPARQLClient.prototype.setKomaQuery = function(komaFirstLast) {
     this.projectID = 'megurunbo';
 };
 
+/**
+ * 食べ物情報を取得するためのSPARQLクエリの生成
+ * @param {Array} array コマに掲載されている食べ物の配列
+ */
 SPARQLClient.prototype.setFoodQuery = function(array) {
     var tmp = "";
     for (var i = 0; i < array.length; i++) {
@@ -31,6 +40,10 @@ SPARQLClient.prototype.setFoodQuery = function(array) {
     console.log(this.sparql);
 };
 
+/**
+ * 場所の情報を取得するためのSPARQLクエリの生成
+ * @param {Array} array コマに掲載されている場所の配列
+ */
 SPARQLClient.prototype.setPlaceQuery = function(array) {
     var tmp = "";
     for (var i = 0; i < array.length; i++) {
@@ -49,6 +62,10 @@ SPARQLClient.prototype.setPlaceQuery = function(array) {
     console.log(this.sparql);
 };
 
+/**
+ * 生成したクエリと指定したプロジェクトでデータを取得
+ * @param  {Method} success 成功時の処理を行う関数
+ */
 SPARQLClient.prototype.request = function(success) {
     this.rdfmgr.executeSparql({
         sparql: this.sparql,
