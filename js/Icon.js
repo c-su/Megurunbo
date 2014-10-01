eb2ac40e877d56c019e6bcfa29755807d8cfb697
Yukihiro Moriyama <m.y.like.music.10@gmail.com>
2014-10-01 16:35:09 +0900

    アイコンへコメント付与

--- js/Icon.js
+++ js/Icon.js
@@ -1,4 +1,10 @@
-function Icon(category, recipe, des) {
+/**
+ * コマに表示するアイコンオブジェクト
+ * @param {string} category 表示するアイコンの種類
+ * @param {string} imageName  画像の名前
+ * @param {string} des      説明文
+ */
+function Icon(category, imageName, des) {
     this.x = 0;
     this.y = 0;
     this.w = width/10;
@@ -6,16 +12,26 @@ function Icon(category, recipe, des) {
     this.tooltip_w = 1000;
     this.tooltip_h = 250;
     this.img = loadImage('./img/icon/' + category + '.png');
-    this.info = loadImage('./img/' + category + '/' + recipe + '.jpg');
+    this.info = loadImage('./img/' + category + '/' + imageName + '.jpg');
     this.des = des;
     this.touchFlag = false;
 }
 
+/**
+ * アイコン位置を指定するための関数
+ * @param {number} x アイコンのX座標
+ * @param {number} y アイコンのY座標
+ */
 Icon.prototype.setPosition = function(x, y) {
   this.x = x;
   this.y = y;
 };
 
+/**
+ * アイコンを表示するための関数
+ * @param  {[type]} posX [description]
+ * @param  {[type]} posY [description]
+ */
 Icon.prototype.display = function(posX, posY) {
     image(this.img, this.x + posX, this.y + posY, this.w, this.h);
 
@@ -31,10 +47,21 @@ Icon.prototype.display = function(posX, posY) {
     }
 };
 
+/**
+ * タッチ判定関数
+ * @param  {number}  current_x タッチしたX座標
+ * @param  {number}  current_y タッチしたX座標
+ * @param  {[type]}  posX      [description]
+ * @param  {[type]}  posY      [description]
+ * @return {Boolean}           タッチ判定
+ */
 Icon.prototype.isTouch = function(current_x, current_y, posX, posY) {
     return (current_x > this.x && current_x < this.x + this.w + posX && current_y > this.y + posY && current_y < this.y + this.h + posY);
 };
 
+/**
+ * ツールチップの表示／非表示を管理するフラグ関数
+ */
 Icon.prototype.setTouchFlag = function() {
     this.touchFlag = !this.touchFlag;
 };
