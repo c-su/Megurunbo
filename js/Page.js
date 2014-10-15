9b1760713a469f27435cdb82005e4e1f7362346a
kanta <santoorino3@yahoo.co.jp>
2014-10-15 23:47:31 +0900

    ツールチップのサイズを縮小

--- js/Page.js
+++ js/Page.js
@@ -115,14 +115,14 @@ Page.prototype.showHideToolTip = function() {
         push();
         console.log("OK");
         imageMode(CENTER);
-        image(this.foodImage, width/2, height/2-width*3/8, width*3/4, width*3/4);
+        image(this.foodImage, width/2, height/2-width*3/12, width*3/6, width*3/6);
         rectMode(CENTER);
         fill(255);
-        rect(width/2, height/2+width*3/8, width*3/4, width*3/4);
+        rect(width/2, height/2+width*3/24, width*3/6, width*3/12);
         fill(0);
         textSize(16);
         var foldedFoodDescription = "";
-        var lineLength = 24;
+        var lineLength = 16;
         for (i=0; i<this.foodDescription.length; i+=lineLength){
             var slicedFoodDescription = "";
             slicedFoodDescription = this.foodDescription.slice(i, i+lineLength);
@@ -130,7 +130,7 @@ Page.prototype.showHideToolTip = function() {
         }
         console.log(foldedFoodDescription);
         rectMode(CORNER);
-        text(foldedFoodDescription, width/2-width*3/8+22, height/2+24, width/2+width*3/8-60, height);
+        text(foldedFoodDescription, width/2-width*3/12+20, height/2+18, width/2+width*3/12-70, height);
         pop();
     }
     else {
